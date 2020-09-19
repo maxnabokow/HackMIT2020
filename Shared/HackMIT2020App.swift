72878a71423360ae4d39b7f5ccfc11ab80ac4b6b
Max Nabokow <max.nabokow@gmail.com>
2020-09-19 00:00:47 -0400

    Initial Commit

---
+++ Shared/HackMIT2020App.swift
@@ -0,0 +1,17 @@
+//
+//  HackMIT2020App.swift
+//  Shared
+//
+//  Created by Max Nabokow on 9/19/20.
+//
+
+import SwiftUI
+
+@main
+struct HackMIT2020App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
